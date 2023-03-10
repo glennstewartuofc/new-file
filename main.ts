71dded443b9421cf84402d31b7df6ac17f81b5e2
glennstewartuofc <127443584+glennstewartuofc@users.noreply.github.com>
2023-03-10 13:43:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    graph("a", 3)
 })
+function graph (btn: string, i: number) {
+    for (let index = 0; index < i; index++) {
+        count = 0
+        for (let index = 0; index < 4; index++) {
+            led.toggle(count, 4 - count)
+            basic.pause(500)
+            led.toggle(count, 4 - count)
+            count += 1
+        }
+    }
+}
