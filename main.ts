1832592ceb37050211e565f2dbfee5e5388b35a0
glennstewartuofc <127443584+glennstewartuofc@users.noreply.github.com>
2023-03-10 13:49:54 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,25 @@ input.onButtonPressed(Button.A, function () {
 function graph (btn: string, i: number) {
     for (let index = 0; index < i; index++) {
         count = 0
-        for (let index = 0; index < 4; index++) {
-            led.toggle(count, 4 - count)
-            basic.pause(500)
-            led.toggle(count, 4 - count)
-            count += 1
+        if (btn == "a") {
+            for (let index = 0; index < 5; index++) {
+                led.toggle(count, 4 - count)
+                basic.pause(100)
+                led.toggle(count, 4 - count)
+                count += 1
+            }
+        } else {
+            if (btn == "b") {
+                for (let index = 0; index < 5; index++) {
+                    led.toggle(count, count)
+                    basic.pause(100)
+                    led.toggle(count, count)
+                    count += 1
+                }
+            }
         }
     }
 }
+input.onButtonPressed(Button.B, function () {
+    graph("b", 4)
+})
